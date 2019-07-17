b7ead002225fa520fe1317eb0c6327bdea17251d
N.S.Devaraj <nsdevaraj@gmail.com>
2019-07-17 11:32:57 +0530

    Delete formulaParserTests.js

--- js/formulaParserTests.js
+++
@@ -1,9 +0,0 @@
-var assert = require("assert");
-describe('formulaParser', function() {
-  describe('#constructor()', function () {
-    it('it should be there when you require it', function () {
-      var p = require('formula-ast');
-      assert( typeof p !== undefined, "not there for me");
-    });
-  });
-});
